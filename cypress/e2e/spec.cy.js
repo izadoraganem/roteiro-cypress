5959bab47c43f84ec0862139b482922c40ac2bb5
izadoraganem <izadoraganem@gmail.com>
2024-12-05 10:54:53 -0300

    adicionando os 3 testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -69,3 +69,60 @@ describe('TODOMvc App', () => {
       .should('have.length', 2);
   });
 });
+
+it('Limpa todas as tarefas concluídas', () => {
+  cy.visit('');
+
+  cy.get('[data-cy=todo-input]')
+    .type('Task A{enter}')
+    .type('Task B{enter}');
+
+  cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+    .first()
+    .click();
+
+  cy.get('[data-cy=clear-completed-btn]')
+    .click();
+
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .should('have.length', 1)
+    .first()
+    .should('have.text', 'Task B');
+});
+
+it('Edita uma tarefa existente', () => {
+  cy.visit('');
+
+  cy.get('[data-cy=todo-input]')
+    .type('Estudar Cypress{enter}');
+
+  cy.get('[data-cy=todos-list] > li')
+    .first()
+    .dblclick();
+
+  cy.get('[data-cy=todo-edit-input]')
+    .clear()
+    .type('Estudar Cypress e Mocha{enter}');
+
+  cy.get('[data-cy=todos-list] > li')
+    .first()
+    .should('have.text', 'Estudar Cypress e Mocha');
+});
+
+it('Marca todas as tarefas como concluídas', () => {
+  cy.visit('');
+
+  cy.get('[data-cy=todo-input]')
+    .type('Task 1{enter}')
+    .type('Task 2{enter}')
+    .type('Task 3{enter}');
+
+  cy.get('[data-cy=toggle-all-checkbox]')
+    .click();
+
+  cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+    .each((checkbox) => {
+      cy.wrap(checkbox).should('be.checked');
+    });
+});
